ea0d2c83408e26843ab7e0ed0c29f902d40e288e
Silikhe <silaswans@gmail.com>
2021-06-21 00:53:13 +0300

    adding jquerry

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+        $("#p1").toggle();
+      });
+
+  });
+
+  $(document).ready(function(){
+    $("#p1").click(function(){
+        $("#img1").toggle();
+      });
+
+  });
